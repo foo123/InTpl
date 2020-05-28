ae82748d0ca9fa5b9f59ba28732dcb7ba73ee32e
Nikos M <workingclasscode@gmail.com>
2020-05-28 21:03:23 +0300

    v.1.0.0 update

--- test/tpl3.php
+++ test/tpl3.php
@@ -1,8 +1,8 @@
 <?php $this->extend(ROOT . "/tpl2.php"); ?>
 
-<?php $this->start("b1"); ?>(3 1)<?php $this->super->block("b1"); ?><?php $this->end("b1"); ?>
+<?php $this->start("b1"); ?>(3 1)<?php $this->super()->block("b1"); ?><?php $this->end("b1"); ?>
 
-<?php $this->start("b2"); ?>(3 2)<?php $this->super->block("b2"); ?><?php $this->end("b2"); ?>
+<?php $this->start("b2"); ?>(3 2)<?php $this->super()->block("b2"); ?><?php $this->end("b2"); ?>
 
-<?php $this->start("b3"); ?>(3 3)<?php $this->super->block("b3"); ?><?php $this->end("b3"); ?>
+<?php $this->start("b3"); ?>(3 3)<?php $this->super()->block("b3"); ?><?php $this->end("b3"); ?>
 
